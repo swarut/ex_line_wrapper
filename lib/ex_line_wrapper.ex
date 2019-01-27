18b9e6a512aee0aad925cff883c0701ffac8aac8
Warut Surapat <s.warut@gmail.com>
2019-01-27 13:58:24 +0900

    First commit.

---
+++ lib/ex_line_wrapper.ex
@@ -0,0 +1,18 @@
+defmodule ExLineWrapper do
+  @moduledoc """
+  Documentation for ExLineWrapper.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExLineWrapper.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ex_line_wrapper_test.exs
@@ -0,0 +1,8 @@
+defmodule ExLineWrapperTest do
+  use ExUnit.Case
+  doctest ExLineWrapper
+
+  test "greets the world" do
+    assert ExLineWrapper.hello() == :world
+  end
+end
