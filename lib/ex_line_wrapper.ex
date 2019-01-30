72e2e735e3fc238cd5c31ea2bc0244d925b84724
Warut Surapat <s.warut@gmail.com>
2019-01-30 23:40:28 +0900

    Add authenticate function.

--- lib/ex_line_wrapper.ex
+++ lib/ex_line_wrapper.ex
@@ -1,18 +1,39 @@
 defmodule ExLineWrapper do
+
+  @oauth_endpoint "https://api.line.me/v2/oauth/accessToken"
+
   @moduledoc """
   Documentation for ExLineWrapper.
   """
 
+
   @doc """
-  Hello world.
+  Acquire access token
 
   ## Examples
-
-      iex> ExLineWrapper.hello()
-      :world
-
+    iex> ExLineWrapper.authenticate("channel_id", "channel_secret")
+    {:ok, %{"access_token" => "xxxxxx"}}
   """
-  def hello do
-    :world
+  def authenticate(channel_id, channel_secret) do
+    header = [
+      {"Content-Type", "application/x-www-form-urlencoded"}
+    ]
+    body = URI.encode_query(%{
+      grant_type: "client_credentials",
+      client_id: channel_id,
+      client_secret: channel_secret
+    })
+    IO.puts("body = #{inspect body}")
+    case HTTPoison.post @oauth_endpoint, body, header do
+      {:ok, resp = %HTTPoison.Response{status_code: 400, body: body}} ->
+        IO.puts("STATUS 400: Unauthorized --- #{inspect resp}")
+        {:ok, body} = Jason.decode(body)
+        {:error, body}
+      {:ok, resp = %HTTPoison.Response{status_code: 200, body: body}} ->
+        IO.puts("SENNNTTTT #{inspect resp}")
+        Jason.decode(body)
+      {:error, err} ->
+        {:error, err}
+    end
   end
 end
