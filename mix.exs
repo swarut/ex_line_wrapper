a5866e54d5d7fac3477d18873ac7d571fb17ed86
Warut Surapat <s.warut@gmail.com>
2019-04-15 23:01:01 +0900

    Update Httpoison version.

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ExLineWrapper.MixProject do
   def project do
     [
       app: :ex_line_wrapper,
-      version: "0.1.4",
+      version: "0.1.5",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
@@ -28,7 +28,7 @@ defmodule ExLineWrapper.MixProject do
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
       {:ex_doc, "~> 0.19", only: :dev, runtime: false},
-      {:httpoison, "~> 1.4"},
+      {:httpoison, "~> 1.5.0"},
       {:jason, "~> 1.1"},
       {:exvcr, "~> 0.10", only: :test}
     ]
